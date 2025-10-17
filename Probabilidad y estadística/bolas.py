ce30b87ba13173ee3bb3a856505ead226ddfc956
Pedrogf03 <pedrogf389@gmail.com>
2025-10-17 19:25:26 +0200

    probabilidad negras

--- Probabilidad y estadística/bolas.py
+++ Probabilidad y estadística/bolas.py
@@ -4,8 +4,9 @@ intentos = 2000000
 
 todas_negras = 0;
 
+bolas = ["B"] * 7 + ["N"] * 5
+
 for _ in range(intentos):
-    bolas = ["B"] * 7 + ["N"] * 5
     muestra = random.sample(bolas, 4)
     if all(b == "N" for b in muestra):
         todas_negras += 1
