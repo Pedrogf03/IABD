9dd27c500dd1fe43bad217ff688b1038916d92a5
Pedrogf03 <pedrogf389@gmail.com>
2025-10-17 19:20:52 +0200

    probabilidad negras

--- Probabilidad y estadística/bolas.py
+++ Probabilidad y estadística/bolas.py
@@ -10,4 +10,4 @@ for _ in range(intentos):
     if all(b == "N" for b in muestra):
         todas_negras += 1
 
-print(f"Ha salido todas negras {todas_negras} veces. {todas_negras}/{intentos} = {todas_negras / intentos}")
+print(f"Ha salido todas negras {todas_negras} veces. {todas_negras}/{intentos} = {((todas_negras / intentos) * 100):.2f}%")
